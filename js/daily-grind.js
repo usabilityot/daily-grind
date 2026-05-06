3884d56161faaddb2e2cd979dd93d300926ee9fd
usabilityot <usabilityot@hotmail.com>
2026-05-05 19:07:45 -0700

    adds coffee attributes

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -1,3 +1,21 @@
+/*
+
+    One unique image, with appropriate and matching content in the alt tag.  
+    A paragraph or two of content that describes the daily item (paragraph must include the name of the highlighted weekday)
+    A unique color that supports the image and paragraph of content
+
+
+    pic - image src
+    alt - the alt tag for the image
+    desc - a description of the coffee
+    day - the day of the week for the coffee
+    color - color associated with coffee
+    name - the name of the coffee
+
+*/
+
+
+
 let myDate = new Date();
 let myDay = myDate.getDay();
 let today = ""
@@ -9,7 +27,15 @@ switch(myDay){
  	break;
 
  	case 2:
-   	today = "Tuesday";
+   	    today = "Tuesday";
+        coffee = {
+            name: "Bubble Tea",
+            pic: "bubble-tea.jpg",
+            alt: "A pic of a yummy bubble tea",
+            color: "pink",
+            day: "Tuesday",
+            desc: `I like me some Bubble Tea!`
+        };
  	break;
 
  	default:
