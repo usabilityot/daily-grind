24dce7cd074c65d7978b5203026ca9ed0a943b3f
usabilityot <usabilityot@hotmail.com>
2026-05-05 18:48:54 -0700

    adds js

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -1,1 +1,20 @@
-alert("hello from daily-grind.js!");
+let myDate = new Date();
+let myDay = myDate.getDay();
+let today = ""
+
+switch(myDay){
+
+ 	case 0:
+    	today =  "Sunday";
+ 	break;
+
+ 	case 2:
+   	today = "Tuesday";
+ 	break;
+
+ 	default:
+    	today = "Something went wrong!";
+
+}
+
+alert(today);
